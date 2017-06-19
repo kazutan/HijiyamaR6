c8dd6a89be0c692f33d4939cbd676b9bac14c684
kazutan <kazu@kz-md.net>
2017-06-19 22:59:54 +0900

    set slide

--- render.R
+++ render.R
@@ -1,2 +1,3 @@
 rmarkdown::render("intro_tubeplayR.Rmd", output_dir = "docs/")
 rmarkdown::render("index.Rmd", output_dir = "docs/")
+rmarkdown::render("rmd_de_github_site.Rmd", output_dir = "docs/")
