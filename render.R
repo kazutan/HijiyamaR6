490f6fea79aac668fb767fe972990fbe8bc8c6c7
kazutan <kazu@kz-md.net>
2017-06-24 12:13:11 +0900

    set all docs

--- render.R
+++ render.R
@@ -1,3 +1,12 @@
 rmarkdown::render("intro_tubeplayR.Rmd", output_dir = "docs/")
 rmarkdown::render("index.Rmd", output_dir = "docs/")
 rmarkdown::render("rmd_de_github_site.Rmd", output_dir = "docs/")
+rmarkdown::render("intro_webshot.Rmd", output_dir = "docs/")
+
+rmarkdown::render("intro_ggplot2.Rmd", output_dir = "docs/")
+rmarkdown::render("intoTheRmarkdown.Rmd", output_dir = "docs/")
+rmarkdown::render("fullpagejs_intro.Rmd", output_dir = "docs/")
+
+
+file.copy("for-revealjs.css", "docs/for-revealjs.css")
+file.copy("for_fullpagejs.css", "docs/for_fullpagejs.css")
